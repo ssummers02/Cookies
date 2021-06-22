5f5c4b32d7bdf2993df1ffa9c190e12f3d8f5271
ssummers <kim-nikita202@mail.ru>
2021-06-22 18:13:13 +0500

    добавлена обработка пользователей и приветственное сообщение

--- bot/bot.go
+++ bot/bot.go
@@ -2,23 +2,86 @@ package bot
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/SevereCloud/vksdk/v2/api"
 	"github.com/SevereCloud/vksdk/v2/api/params"
 	"github.com/SevereCloud/vksdk/v2/events"
 	"github.com/SevereCloud/vksdk/v2/longpoll-bot"
+	"io/ioutil"
 	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"time"
 )
 
+// Отправка сообщения пользователю
 func createAndSendMessages(vk *api.VK, PeerID int, text string) {
+	rand.Seed(time.Now().UnixNano())
 	b := params.NewMessagesSendBuilder()
 	b.Message(text)
-	b.RandomID(int(rand.Int31()))
+	b.RandomID(rand.Intn(2147483647))
 	b.PeerID(PeerID)
 	_, err := vk.MessagesSend(b.Params)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+type Users struct {
+	LastMessages string `json:"LastMessages"`
+	Cabinet      int    `json:"Cabinet"`
+}
+
+// Exists reports whether the named file or directory exists.
+func Exists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+// Создание файла пользователя
+func createNewUser(nameFile string) {
+	file, err := os.Create(nameFile) // создаем файл
+
+	if err != nil { // если возникла ошибка
+		log.Print("Unable to create file:", err)
+	}
+	emp := &Users{"", 0} // default значения
+	e, err := json.Marshal(emp)
+	file.WriteString(string(e))
+
+	defer file.Close()
+}
+
+func OpenUserFile(nameFile string) Users {
+	var user Users
+
+	jsonFile, err := os.Open(nameFile) // Открытие jsonFile
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile) //Считывание и раскодирование в json
+	json.Unmarshal(byteValue, &user)
+	return user
+
+}
+func changeUserFile(nameFile string, users Users) {
+	file, err := os.Create(nameFile) // создаем файл
+
+	if err != nil { // если возникла ошибка
+		log.Print("Unable to create file:", err)
+	}
+
+	e, err := json.Marshal(users)
+	file.WriteString(string(e))
+
+	defer file.Close()
 }
 func Start() {
 	vk := api.NewVK("aa6f5be89eb316d1fbdfb1fab2d82a8229aec785fa980bdc51d606e03b36b1ec5f740cee6cd56d132efce")
@@ -27,13 +90,34 @@ func Start() {
 		panic(err)
 	}
 
+	//Обработка новых сообщений
 	lp.MessageNew(func(ctx context.Context, obj events.MessageNewObject) {
-		log.Printf("%d:%s", obj.Message.PeerID, obj.Message.Text)
+		Message := obj.Message.Text
 		PeerID := obj.Message.PeerID
 
-		if obj.Message.Text == "ping" {
-			createAndSendMessages(vk, PeerID, "tesst")
+		log.Printf("New Users: %d:%s", PeerID, Message)
+
+		nameFile := "temp/" + strconv.Itoa(PeerID) + ".json"
+		if !Exists(nameFile) {
+			createNewUser(nameFile)
+			createAndSendMessages(vk, PeerID, "Привет! Я Печенька")
+
 		}
+		user := OpenUserFile(nameFile)
+		log.Print(user)
+
+		if user.Cabinet == 0 && user.LastMessages == "Кабинет" {
+			// Добавить проверку на инт
+			createAndSendMessages(vk, PeerID, "Твой новый кабинет:"+Message)
+			user.Cabinet, _ = strconv.Atoi(Message)
+		}
+
+		if user.Cabinet == 0 && user.LastMessages != "Кабинет" {
+			createAndSendMessages(vk, PeerID, "Я тебя не знаю, давай познакомимься поближе\nУкажи номер своего кабинета")
+			user.LastMessages = "Кабинет"
+		}
+
+		changeUserFile(nameFile, user)
 
 	})
 
