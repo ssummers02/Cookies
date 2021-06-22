c1cb319ca78afbea37176e031e64350dfbcdf446
ssummers <kim-nikita202@mail.ru>
2021-06-22 18:18:08 +0500

    Добавлено создание временной папки при инициализации

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"os"
 	"ssummers02/Cookies/bot"
 	"ssummers02/Cookies/db"
 )
 
 func main() {
+	if _, err := os.Stat("temp"); os.IsNotExist(err) {
+		os.Mkdir("temp", 0777)
+	}
 	db.InitDB()
 	bot.Start()
 }
