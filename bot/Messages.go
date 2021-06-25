6d6a27114ec0c072355c39e320a61c437279cb82
ssummers <kim-nikita202@mail.ru>
2021-06-25 20:30:55 +0500

    refactor

---
+++ bot/Messages.go
@@ -0,0 +1,40 @@
+package bot
+
+import (
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/SevereCloud/vksdk/v2/api"
+	"github.com/SevereCloud/vksdk/v2/api/params"
+	"github.com/SevereCloud/vksdk/v2/object"
+)
+
+// Отправка сообщения пользователю
+func PostAndSendMessages(vk *api.VK, PeerID int, text string) {
+	rand.Seed(time.Now().UnixNano())
+	b := params.NewMessagesSendBuilder()
+
+	b.Message(text)
+	b.RandomID(rand.Intn(2147483647))
+	b.PeerID(PeerID)
+	_, err := vk.MessagesSend(b.Params)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Отправка сообщения пользователю
+func PostMessagesAndKeyboard(vk *api.VK, PeerID int, text string, k *object.MessagesKeyboard) {
+	rand.Seed(time.Now().UnixNano())
+	b := params.NewMessagesSendBuilder()
+
+	b.Keyboard(k)
+	b.Message(text)
+	b.RandomID(rand.Intn(2147483647))
+	b.PeerID(PeerID)
+	_, err := vk.MessagesSend(b.Params)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
