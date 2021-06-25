7f79440b09231a840cc09b02046b4375261a7b4a
Anton Bobrik <anton@bobrik.org>
2021-06-25 12:59:32 +0400

    add: handlers for ActiveTasks, AllTasks, Settings web pages

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"ssummers02/Cookies/api"
 	"ssummers02/Cookies/bot"
 	"ssummers02/Cookies/db"
+	"ssummers02/Cookies/web"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
@@ -42,6 +43,10 @@ func main() {
 	router.HandleFunc("/api/task/{id:[0-9]+}", api.PutTask).Methods("PUT")
 	router.HandleFunc("/api/user/{user_id:[0-9]+}/{count:[0-9]+}", api.GetHistory).Methods("GET")
 	router.HandleFunc("/api/task/{task_id:[0-9]+}/{status:[0-9]+}", api.ChangeStatus).Methods("PUT")
+
+	router.HandleFunc("/", web.ShowActiveTasks).Methods("GET")
+	router.HandleFunc("/all", web.ShowAllTasks).Methods("GET")
+	router.HandleFunc("/settings", web.ShowSettings).Methods("GET")
 
 	srv := &http.Server{
 		Handler: router,
